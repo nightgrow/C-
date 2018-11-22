891d04d3bc25604d170210e95895652f0afe266f
nightgrow <ion2141@naver.com>
2018-11-22 15:55:21 +0900

    chapter10_c++

---
+++ chapter10/10_9_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class AAA {
+	int value;
+	friend class BBB;
+	
+	public:
+		AAA(int v) {
+			value = v;
+		}
+		
+		void Show() {
+			cout << value << endl;
+		}
+};
+
+class BBB {
+	public:
+		int Initial(AAA a1) {
+			return a1.value;
+		}
+};
+
+int main() {
+	AAA aa1(10);
+	BBB bb1;
+	
+	cout << bb1.Initial(aa1) << endl;
+	aa1.Show();
+}
